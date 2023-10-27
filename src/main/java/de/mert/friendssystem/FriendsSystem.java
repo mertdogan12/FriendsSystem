bbd155a5cccc1de479ea18104eb92cd6fc547f26
Mert Dogan <mert.dogan@me.com>
2023-10-27 22:39:54 +0200

    First commit

---
+++ src/main/java/de/mert/friendssystem/FriendsSystem.java
@@ -0,0 +1,11 @@
+package de.mert.friendssystem;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FriendsSystem extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
