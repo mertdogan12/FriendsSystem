6119e267bc7ee06892ee4291eedc113d6b6780f2
Mert Dogan <mert.dogan@me.com>
2023-10-29 18:25:33 +0100

    table unique added

--- src/main/resources/dbsetup.sql
+++ src/main/resources/dbsetup.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `friends`.`friends`
 (
     `player1` UUID NOT NULL ,
-    `player2` UUID NOT NULL
+    `player2` UUID NOT NULL,
+    UNIQUE (`player1`, `player2`)
 )
 ENGINE = InnoDB;
